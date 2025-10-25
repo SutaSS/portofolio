91d9eefe853316c3a3a3dd20c93afafdf9a1e6f0
Andikasaktih <andikasaktih@gmail.com>
2025-10-25 21:27:32 +0700

    refactor(techStack) : "don grid, tinggal nyari gambar"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Navbar";
 import About from "./section/about";
 import TechStack from "./section/techStack";
-import Project from "./section/Project";
+import Project from "./section/project";
 import Hero from "./section/Hero";
 
 export default function Home() {
